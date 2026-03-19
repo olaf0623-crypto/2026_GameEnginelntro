b4d77d578deedbc659ba226f9c50234d4643f58e
olaf0623-crypto <olaf0623@g.shingu.ac.kr>
2026-03-19 15:54:43 +0900

    플레이어 점프 및 애니메이션

--- My project/Assets/PlayerController.cs
+++ My project/Assets/PlayerController.cs
@@ -4,10 +4,31 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Vector2 moveInput;
+    public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 
     // Update is called once per frame
@@ -22,9 +43,21 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(-1 , 1, 1);
         }
+
+        if(moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
         transform.Translate(Vector3.right * moveInput.x * Time.deltaTime);
 
+
+
     }
+
 
 
 }
